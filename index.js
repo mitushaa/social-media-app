5e9dd2af0be7518ffedb7ef05968ad81da710e9a
mitushaa <aryamitusha@gmail.com>
2020-11-23 20:37:15 +0530

    graphql folder

--- index.js
+++ index.js
@@ -2,46 +2,21 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag'); 
 const mongoose = require('mongoose');
                            
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 const Post = require('./models/Post');
 const { MONGODB } = require('./config.js');
-
-//GraphQL types 
-const typeDefs = gql`
-    type Post{
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-    type Query{
-        getPosts: [Post]                                         
-    }
-`;
-const resolvers = {
-    Query: {
-        async getPosts() {
-            try {
-                const posts = await Post.find();
-                return posts;
-            } catch (err) {
-                throw new Error(err);
-            }       
-        }
-    }   
-};
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
 
-//Connecting to mongoDB
-
 mongoose
    .connect(MONGODB, { useNewUrlParser: true})
    .then(() => {
        return server.listen({ port: 5000});
-   })
+})
     .then(res => {
-        console.log('Server running at ${res.url}')          //The port where running Eg port 5000
-    });
+        console.log('Server running at ${res.url}') 
+});
