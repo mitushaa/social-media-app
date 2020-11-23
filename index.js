c31c6dc8c5cdee1d0d33e657e5a8f10c982a3dad
mitushaa <aryamitusha@gmail.com>
2020-11-23 15:24:35 +0530

    Post.js & User.js

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');                             //Dependency of Apollo Server
+const gql = require('graphql-tag'); 
+const mongoose = require('mongoose');
+                           
+const Post = require('./models/Post');
+const { MONGODB } = require('./config.js');
 
-                                                                //GraphQL types
-                                                                //Always required
+//GraphQL types 
 const typeDefs = gql`
-    type Query{
-        sayHi: String!                                          
-
+    type Post{
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
     }
-`
+    type Query{
+        getPosts: [Post]                                         
+    }
+`;
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World!'
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }       
+        }
     }   
-}
+};
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
 
-server.listen({ port: 5000})
+//Connecting to mongoDB
+
+mongoose
+   .connect(MONGODB, { useNewUrlParser: true})
+   .then(() => {
+       return server.listen({ port: 5000});
+   })
     .then(res => {
-        console.log('Server running at ${res.url}')
-    })
+        console.log('Server running at ${res.url}')          //The port where running Eg port 5000
+    });
